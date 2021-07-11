c15b2c37dcf0fac2d40421e025f5fe2268c54174
EddieKiller <nataniel.palacios@alumnos.uv.cl>
2021-07-10 22:48:47 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,39 +1,39 @@
-#buscar regiones, regiones cod, comunas, comunas cod.
+archivo = open("C:\\Users\eddie\OneDrive\Documentos\Archivos.txt\CasosPorComuna.csv.txt", "r")
 
-archivo = open("C:\\Users\eddie\OneDrive\Documentos\Archivos.txt\CasosActivosPorComuna.csv", "r")
+linea1 = archivo.readline() #lee primera linea
+linea1 = linea1.strip()     #quita el salto de linea de la primera linea
+linea1 = linea1.split(",")  #transforma cada elemento de primera linea separado por una coma, en un elemento de la lista
 
 lineas = archivo.readlines() #lee cada linea y las almacena en una lista, cada elemento de la lista es una linea
-#print(lineas)
-regiones = []
-regionescod = []
-comunas = []
-comunascod = []
+
+regiones = []           
+regionescod = []        #Listas donde se almacenarán#
+comunas = []             #comunas y códigos de comunas#
+comunascod = []         #para posterior verificación#
 
 #ciclo que recorre cada elemento de la lista lineas
 for linea in lineas:
-    linea = linea.strip()                #borramos el salto de linea      
-    #print(linea)                        #imprimimos la linea
+    linea = linea.strip("\n")            #borramos el salto de linea      
     listalinea = linea.split(",")        #encerramos la linea en una lista
-    #print(listalinea)                   #imprimimos la lista que corresponde a toda una linea con sus elementos separados
 
     for elemento in range(len(listalinea)):     #ciclo for que recorre el largo de la listalinea que contiene todos los elementos de una linea
 
         if elemento == 0 and listalinea[elemento].lower() not in regiones:          #si el indice es igual a 0, o sea, si reconoce el primer elemento de la lista
             regiones.append(listalinea[elemento].lower())                           #añadir a la lista regiones
         
-        if elemento == 1 and listalinea[elemento] not in regionescod:       #si el indice es igual a 1
-          regionescod.append(listalinea[elemento])                          #añadir a lista de código de regiones
+        if elemento == 1 and listalinea[elemento] not in regionescod:               #si el indice es igual a 1
+          regionescod.append(listalinea[elemento])                                  #añadir a lista de código de regiones
         
-        if elemento == 2:                                                   #si el indice es igual a 2
+        if elemento == 2:                                                           #si el indice es igual a 2
             comunas.append(listalinea[elemento].lower())                            #añadir a lista de comunas
         
-        if elemento == 3:                                                   #si el indice es igual a 3
-            comunascod.append(listalinea[elemento])                         #añadir a lista de código de regiones
+        if elemento == 3:                                                           #si el indice es igual a 3
+            comunascod.append(listalinea[elemento])                                 #añadir a lista de código de regiones
         
-print(regiones, "\n")
-print(regionescod, "\n")
-print(comunas, "\n")
-print(comunascod, "\n")
+#print(regiones, "\n")          
+#print(regionescod, "\n")   
+#print(comunas, "\n")           
+#print(comunascod, "\n")    
 
 archivo.close()
 
@@ -41,7 +41,7 @@ archivo.close()
 
 print("----------------------------Bienvenido----------------------------")
 
-region = input("""------------------------------------------------------------------
+print("""------------------------------------------------------------------
 
 15. Región de Arica y Parinacota
 01. Región de Tarapacá
@@ -62,67 +62,42 @@ region = input("""--------------------------------------------------------------
 
 ------------------------------------------------------------------
 
-Ingrese por nombre o por código la región que desea buscar: 
 """)
-
-while not ((region.lower() in regiones) or (region in regionescod)):            #verificar si la región está en la lista de regiones
-    region = input("ERROR, ingrese región nuevamente: ")                        # o lista de códigos de regiones
-
-print("")
 
 comuna = input("Ingrese la comuna que desea buscar: ")                          
 
 while not ((comuna.lower() in comunas) or (comuna in comunascod)):              #verificar si la región está en la lista de regiones
-    comuna = input("ERROR, ingrese comuna nuevamene: ")                         # o lista de códigos de regiones
+    comuna = input("ERROR, ingrese una comuna válida: ")                        # o lista de códigos de regiones
 
-#comentarios hola a todos ajdhakjdhakdahd
+fecha = input("Ingrese una fecha: ")                            
 
-'''
-archivo = open("C:\\Users\eddie\OneDrive\Documentos\Archivos.txt\CasosPorComuna.csv", "r")
+while not fecha in linea1:                                                      #verificar si la fecha está en la lista de fechas
+    fecha = input("ERROR, ingrese una fecha válida: ")                         
 
-linea1 = archivo.readline() #lee primera linea
-linea1 = linea1.strip()     #quita el salto de linea de la primera linea
-linea1 = linea1.split(",")  #transforma cada elemento de primera linea separado por una coma, en un elemento de la lista
-
-
-lineas = archivo.readlines() #lee cada linea y las almacena en una lista, cada elemento de la lista es una linea
-#print(lineas)
-
-comuna = input("ingrese la comuna que desea buscar: ")
-fecha = input("ingrese una fecha: ")
 contagiados = 0
-print(linea1)
 
-#ciclo que recorre cada elemento de la lista lineas
 for linea in lineas:
     linea = linea.strip("\n")           #borramos el salto de linea      
-                                        #imprimimos la linea
 
     listalinea = linea.split(",")       #guardamos cada elemento de la linea en una lista
-    print(listalinea)                   #imprimimos la lista que corresponde a toda una linea con sus elementos separados
 
     for elemento in range (len(listalinea)): #se recorre cada elemento de la linea actual
 
         if listalinea[elemento].lower() == comuna.lower(): #si el elemento actual es igual a la comuna ingresada por el usuario
-            print(listalinea[elemento])                    #imprimir comuna
+            #print(listalinea[elemento])                    #imprimir comuna
 
-##################################################
+##############################################################################################################################################################3
 
             for buscar in range (len(linea1)):              #ciclo for que empiece a recorrer todas los elementos(fechas) de la primera linea
                 
                 if linea1[buscar] == fecha:                 #si la fecha que ingreso el usuario coincide con alguna de la lista
                     
-                    for contag in range (len(listalinea)):
-                        
-                        if contag == buscar:
-                            #print("")
-                            #print(listalinea[contag])
-                            #print("")
-
-                            contagiados = listalinea[contag]
+                    for contag in range (len(listalinea)):  #ciclo for que recorre nuevamente la linea en que se encuentra, ahora
+                                                            #con el objetivo de buscar los contagiados
+                        if contag == buscar:                    #si el indice del contagiado es igual al indice de la fecha en nuestra lista linea1
+                            contagiados = listalinea[contag]    #guardamos la cantidad de contagiados en nuestra variable del mismo nombre.   
                             
-if contagiados == "":
-    contagiados = 0
+if contagiados == "":   #si el número de contagiados es vacío
+    contagiados = 0     #reemplazar por un 0
 
-print("La cantidad de contagiados en la comuna de", comuna, "para la fecha", fecha, "es: ", contagiados)
-'''
+print("La cantidad de casos activos en la comuna de", comuna.upper(), "para la fecha", fecha, "es: ", contagiados) #verificación de datos por pantalla
